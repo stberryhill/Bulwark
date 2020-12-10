0018fbf5a0e6993e170cf20edb9c908e4000bd60
S. Tyler Berryhill <sean.tyler.berryhill@gmail.com>
2020-12-09 22:02:19 -0600

    Restoring event queue changes part 2
    Final commit restoring past deleted code. Will now work on making input asynchronous.
    This reverts commit ee6e5322e7197d39936aca51e97f33f9e7fd1246.

--- src/Bulwark.c
+++ src/Bulwark.c
@@ -22,6 +22,7 @@ static const char ANSI_EXIT_ALTERNATE_BUFFER_MODE[] = "?1049l";
 static const char ANSI_CLEAR_BUFFER_AND_KILL_SCROLLBACK[] = "2J";
 static const char ANSI_HIDE_CURSOR[] = "\x1b[?25h";
 static const char ANSI_SHOW_CURSOR[] = "\x1b[?25l";
+static BulwarkEventQueue *eventQueue;
 static struct termios termiosBeforeQuickTermInitialized;
 static int windowWidth;
 static int windowHeight;
@@ -31,6 +32,10 @@ static FILE *logFileDescriptor;
 static void storeTerminalSettingsSoWeCanRestoreThemWhenWeQuit();
 static void readInitialWindowWidthAndHeight();
 static void enableRawInput();
+static void initializeEventQueue();
+static void setupTerminalToSendResizeEventsWhenResized();
+static void installTerminalResizeCallback();
+static void terminalResizeCallback(int i);
 static void prepareBuffer();
 static void enterAlternateBufferSoWeDontMessUpPastTerminalHistory();
 static void exitAlternateBufferModeSinceWeEnteredUponInitialization();
@@ -50,6 +55,8 @@ void Bulwark_Initialize() {
   readInitialWindowWidthAndHeight();
   enableRawInput();
   prepareBuffer();
+  initializeEventQueue();
+  setupTerminalToSendResizeEventsWhenResized();
 }
 
 static void storeTerminalSettingsSoWeCanRestoreThemWhenWeQuit() {
@@ -92,6 +99,37 @@ static void disableBufferingOnStdoutSoPrintfWillGoThroughImmediately() {
   setvbuf(stdout, NULL, _IONBF, 0);
 }
 
+static void initializeEventQueue() {
+  eventQueue = BulwarkEventQueue_Create();
+}
+
+static void setupTerminalToSendResizeEventsWhenResized() {
+  installTerminalResizeCallback();
+}
+
+static void installTerminalResizeCallback() {
+  /* Our child process will receive the SIGWINCH signal whenever the terminal is resized */
+  signal(SIGWINCH, terminalResizeCallback);
+}
+
+static void terminalResizeCallback(int i) {
+  struct winsize newWindowSize;
+
+	ioctl(1, TIOCGWINSZ, &newWindowSize);
+
+  /* Set these so GetWindowWidth and GetWindowHight functions stay up-to-date. */
+	windowWidth = newWindowSize.ws_col;
+	windowHeight = newWindowSize.ws_row;
+
+  /* Queue window resize event */
+  BulwarkEvent event;
+  event.type = BULWARK_EVENT_TYPE_WINDOW_RESIZE;
+  event.newWindowWidth = windowWidth;
+  event.newWindowHeight = windowHeight;
+
+  BulwarkEventQueue_AddEvent(eventQueue, &event);
+}
+
 void Bulwark_Quit() {
   clearBufferAndKillScrollback();
   exitAlternateBufferModeSinceWeEnteredUponInitialization();
@@ -107,8 +145,15 @@ static void restoreTerminalSettingsToWhatTheyWereBeforeWeInitialized() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &termiosBeforeQuickTermInitialized);
 }
 
+bool Bulwark_HasEventsInQueue() {
+  return !BulwarkEventQueue_IsEmpty(eventQueue);
+}
+
+void Bulwark_ReadNextEventInQueue(BulwarkEvent *output) {
+  BulwarkEventQueue_ReadAndConsumeEvent(eventQueue, output);
+}
+
 void Bulwark_WaitForNextEvent(BulwarkEvent *output) {
-  fflush(logFileDescriptor);
   if (read(STDIN_FILENO, &output->character, 1) < 0) {
     fprintf(logFileDescriptor, "Error - could not read character from STDIN_FILENO\n");
     exit(-1);
