7f3484f2ecfc7473f2fef6e39712a35f5c923a68
S. Tyler Berryhill <sean.tyler.berryhill@gmail.com>
2021-03-08 13:09:34 -0600

    Feat: Implement buffer resize function.

--- src/Buffer.c
+++ src/Buffer.c
@@ -31,29 +31,47 @@ void Buffer_Initialize(const uint16_t width, const uint16_t height) {
 }
 
 void Buffer_Resize(const uint16_t width, const uint16_t height) {
+  /* First, resize height */
   if (buffer->height > height) {
+    /* 1. Free char arrays that will be removed */
     int j;
     for (j = height; j < buffer->height; j++) {
       free(buffer->characters[j]);
     }
 
+    /* 2. Remove extra character arrays to reach new smaller size */
     buffer->characters = realloc(buffer->characters, height);
 
   } else if (buffer->height < height) {
+    /* 1. Reallocate to include new rows */
     buffer->characters = realloc(buffer->characters, height);
 
     int j;
     for (j = buffer->height; j < height; j++) {
+      /* 2. Allocate row characters */
+      buffer->characters[j] = malloc(buffer->width * (sizeof *buffer->characters[j]));
+
       int i;
       for (i = 0; i < buffer->width; i++) {
+        /* 3. Set new characters to clear */
         buffer->characters[j][i] = CLEAR_CHARACTER;
         buffer->colorCodes[j][i] = CLEAR_COLOR_CODE;
       }
     }
   }
 
-  buffer->width = width;
   buffer->height = height;
+
+  /* Next resize width */
+  if (buffer->width != width) {
+    int j;
+    for (j = 0; j < height; j++) {
+      /* Just a simple realloc */
+      buffer->characters[j] = realloc(buffer->characters[j], width * (sizeof *buffer->characters[j][0]));
+    }
+  }
+
+  buffer->width = width;
 }
 
 uint8_t Buffer_GetColorCodeAtPosition(const uint16_t x, const uint16_t y) {
